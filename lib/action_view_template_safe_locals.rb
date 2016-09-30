390d166615fb1580a0beca43d4c790f527d42e7c
Peter Schilling <peter@peterschilling.org>
2016-09-30 09:11:32 -0700

    Update list of ruby keywords

--- lib/action_view_template_safe_locals.rb
+++ lib/action_view_template_safe_locals.rb
@@ -3,10 +3,13 @@ require "action_view"
 
 # https://github.com/rails/rails/blob/master/actionview/lib/action_view/template.rb#L326
 module ActionViewTemplateSafeLocals
-  RESERVED_WORDS = %w(class return super if unless)
+  RUBY_KEYWORDS = %w(begin end alias and begin break case class def do else
+                     elsif end ensure false for if in module next nil not or
+                     redo rescue retry return self super then true undef unless
+                     until when while yield)
 
   def locals_code
-    locals = @locals.delete_if { |key| RESERVED_WORDS.include?(key.downcase) }
+    locals = @locals.delete_if { |key| RUBY_KEYWORDS.include?(key.downcase) }
     locals.each_with_object("") { |key, code| code << "#{key} = #{key} = local_assigns[:#{key}];" }
   end
 end
